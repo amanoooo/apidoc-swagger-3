62fe3496fec7dc017df70538e0dfed9eb71b042b
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-03-10 16:35:35 +0100

    Moving from apidoc-core lib to apidoc lib

--- lib.js
+++ lib.js
@@ -1,13 +1,10 @@
-
 var app = {
     options: {}
 }
-var apidoc = require('apidoc-core')
+var apidoc = require('apidoc')
 var winston = require('winston');
 
 const apidoc_to_swagger = require('./apidoc_to_swagger');
-apidoc.setGeneratorInfos({ name: 'name', time: new Date(), version: '0.0.1', url: 'xxx url' })
-
 
 function generateLog() {
     var log = winston.createLogger({
@@ -26,40 +23,33 @@ function generateLog() {
 }
 
 function main(options) {
-
     app.options = options
     app.options.verbose && console.log('options', app.options);
     generateLog()
-    const { src, dest, verbose } = options
-    apidoc.setLogger(app.options.log)
+    options.verbose && console.log('options', options);
 
-    var api = apidoc.parse({ ...app.options, log: app.options.log })
+    var api = apidoc.createDoc({ ...app.options, log: app.options.log })
 
     if (app.options.parse !== true) {
         var apidocData = JSON.parse(api.data);
         var projectData = JSON.parse(api.project);
 
         const swagger = apidoc_to_swagger.toSwagger(apidocData, projectData)
-
         api["swaggerData"] = JSON.stringify(swagger);
         createOutputFile(api, app.options.log)
     }
 }
 
-
 const fs = require('fs')
 const path = require('path')
 function createOutputFile(api, log) {
-    if (app.options.simulate)
-        log.warn('!!! Simulation !!! No file or dir will be copied or created.');
-
     log.verbose('create dir: ' + app.options.dest);
-    if (!app.options.simulate)
+    if (!app.options.dryRun)
         fs.existsSync(app.options.dest) || fs.mkdirSync(app.options.dest);
 
     //Write swagger
     log.verbose('write swagger json file: ' + app.options.dest + 'swagger.json');
-    if (!app.options.simulate)
+    if (!app.options.dryRun)
         fs.writeFileSync(app.options.dest + './swagger.json', api.swaggerData);
 
 }
